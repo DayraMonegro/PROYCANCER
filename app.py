6419675e06f2a73f6fe7f9c0618b2dc6a545c38e
DayraMonegro <dayramonegro9@gmail.com>
2025-06-04 08:39:04 -0400

    04.6.25.23

--- app.py
+++ app.py
@@ -286,5 +286,8 @@ def cancer_metrics():
         logger.error(f"Error retrieving cancer metrics: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+##app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto dinámicamente
+    app.run(host='0.0.0.0', port=port)
+
+    
